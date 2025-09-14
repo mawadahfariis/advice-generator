943139834f58d82a54deedb0b9ba4b957ba0c6cc
mawadahfariis <mwadahfariis3@gmail.com>
2025-09-14 17:04:26 +0300

    advice generator done

---
+++ AGG/src/AdviceCard.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const AdviceCard = () => {
+    const [advice, setadvice] = useState();
+    const [loading, setloading] = useState(false);
+    const [idd, setid] = useState(null);
+
+
+    const FetchAdvice = () => {
+        setloading(true);
+        fetch("https://api.adviceslip.com/advice")
+            .then((res) => res.json())
+            .then((data) => 
+                {setadvice(data.slip.advice);
+                    setid(data.slip.id);
+                }
+                
+                )
+            .catch(()=>setadvice("error,try again :("))
+            .finally(() => setloading(false));
+
+    };
+    useEffect(() => {
+        FetchAdvice();
+    }, [])
+    return (
+        <div className="bg-gray-400 text-white p-8 rounded-xl shadow-lg text-center relative max-w-md w-full mx-auto">
+            <p className="text-pink-800 text-sm tracking-wider mt-1 mb-1">
+                {idd ? `Advice #${idd}` : "Advice Generator"}
+            </p>
+            <p className="text-2xl mb-3 md:text-3xl italic">
+                {loading ? "loading..." : `"${advice}"`}
+            </p>
+            <img src="../src/assets/pattern-divider-desktop.svg" className="mx-auto mt-6 mb-7 "></img>
+            <button
+                className=" active:scale-90 hover:cursor-pointer bg-pink-800 p-4 rounded-full shadow-lg  hover:shadow-[0_0_40px_15px_pink]"
+                onClick={FetchAdvice}
+            >
+                <img src="../src/assets/icon-dice.svg" alt="dice to roll"></img>
+            </button>
+
+        </div>
+    );
+}
+
+export default AdviceCard;
---
+++ AGG/src/App.jsx
@@ -0,0 +1,13 @@
+import AdviceCard from './AdviceCard';
+import './App.css'
+
+function App() {
+
+    return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-600">
+      <AdviceCard/>
+    </div>
+  );
+}
+
+export default App
